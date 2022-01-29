3489709a55be8ea0b7e5380cdd452676ecf98276
todano <g.todor.g@gmail.com>
2022-01-29 17:14:47 +0200

    jason
    fix composer,jason error

--- index.php
+++ index.php
@@ -2,43 +2,36 @@
 session_start();
 error_reporting(E_ALL);
 ini_set('display_errors',1);
+// ==============define constants ======================================
 DEFINE('DS',DIRECTORY_SEPARATOR);
 DEFINE('BASE_URL','http://localhost/PHPoop/final/catalogRyt/');
 DEFINE('DEFAULT_CONTROLLER','Main');
 DEFINE('DEFAULT_METHOD','index');
 DEFINE('BASE_PATH',__DIR__);
 DEFINE('CONTROLLER_PATH',BASE_PATH.DS.'src'.DS.'Controllers'.DS);
+//=========================================================================
 require_once 'vendor/autoload.php';
 require_once 'src/Helpers/functions.php';
-
+// ================ get url ===============================
 $url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 $url = str_replace(BASE_URL,'',$url);
 $urlParts = array_filter(explode('/',$url));
+// get controller from url =========================
 $controller = $urlParts[0] ?? DEFAULT_CONTROLLER;
 $controller = ucFirst(strtolower($controller));
 $controllerFile = $controller.'.php';
-// echo '<pre>'; print_r($controller);  die;
+// ==== get method from url ==================================
 $method = $urlParts[1] ?? DEFAULT_METHOD;
 unset($urlParts[0],$urlParts[1]);
+// ========= ste method to default if there is no method
 if(!file_exists(CONTROLLER_PATH.$controllerFile))
 {
   $controller = DEFAULT_CONTROLLER;
 }
+// ======= initiate new object in controller ==============
 $initController = "Tod\\Controllers\\".$controller;
 $app = new $initController();
 
-echo go('login', 'index');
-//call method from the object with params
+// ======= call method from the object with params ==============
 $con = Tod\Helpers\Database::getConnection();
 call_user_func_array(array($app, $method), $urlParts);
-//include 'header.php';
-
-// $sql = "SELECT * FROM `users`";
-// $query = $con->prepare($sql);
-// $query->execute();
-// $users = $query->fetchAll();
-// echo '<pre>'; print_r($users);
-
-
-
-?>
